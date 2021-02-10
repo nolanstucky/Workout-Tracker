f821f52c751efc6d39ce4e2885b41b62f2c28f61
Nolan Stucky <nolan@stucky.net>
2021-02-09 19:06:29 -0800

    finished workout model

--- models/Workout.js
+++ models/Workout.js
@@ -3,11 +3,43 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    Workoutname: String,
-
-    password: {
-        type: string
+    day: {
+        type: Date,
+        default: Date.now
     },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name: {
+                type: String,
+                trim: true,
+            },
+            duration: Number,
+            weight: {
+                type: Number,
+                default: 0
+            },
+            reps: {
+                type: Number,
+                default: 0
+            },
+            sets: {
+                type: Number,
+                default: 0
+            },
+            distance: {
+                type: Number,
+                default: 0,
+            },
+        }
+    ],
+    duration: {
+        type: Number,
+        default: 0
+    }
 })
 
 const Workout = mongoose.model("Workout", WorkoutSchema)
